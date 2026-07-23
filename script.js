65fcab3762302e247de1c85a84c2b0a96bd55500
ybhavyasree4 <bhavyasreey54@gmail.com>
2026-07-23 15:02:45 +0530

    Cricket

---
+++ script.js
@@ -0,0 +1,14 @@
+// Welcome Message
+console.log("Cricket Tournament Management System");
+
+// Login Button 
+
+function loginSuccess() {
+    alert("Login Successful!");
+}
+
+
+// Match Result 
+function showWinner() {
+    alert("Winner : RCB");
+}
